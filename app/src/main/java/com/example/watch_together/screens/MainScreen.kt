8935947e324d50b86688144d1487189ca20c509b
Daniil Bobkov <Lurkingoose@gmail.com>
2025-02-24 17:52:05 +0600

    Added new features and fixed bugs

--- app/src/main/java/com/example/watch_together/screens/MainScreen.kt
+++ app/src/main/java/com/example/watch_together/screens/MainScreen.kt
@@ -2,10 +2,13 @@ package com.example.watch_together.screens
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -38,11 +41,37 @@ fun MainScreen(
             )
         }
     ) { paddingValues ->
-        when (selectedTab) {
-            Screen.Search -> SearchTabNavHost(movieViewModel, favoritesViewModel, paddingValues)
-            Screen.Favorites -> FavoritesTabNavHost(movieViewModel, favoritesViewModel, paddingValues, savedMovieId, onMovieIdSaved = { savedMovieId = it })
-            Screen.Settings -> SettingsScreen(movieViewModel, authViewModel, paddingValues)
+        Box(modifier = Modifier.padding(paddingValues)) {
+            AnimatedContent(
+                targetState = selectedTab,
+                label = "MainScreen_AnimatedContent"
+            ) { currentScreen ->
+                when (currentScreen) {
+                    Screen.Search -> key("search") {
+                        SearchTabNavHost(
+                            movieViewModel = movieViewModel,
+                            favoritesViewModel = favoritesViewModel,
+                            paddingValues = paddingValues
+                        )
+                    }
+                    Screen.Favorites -> key("favorites") {
+                        FavoritesTabNavHost(
+                            movieViewModel = movieViewModel,
+                            favoritesViewModel = favoritesViewModel,
+                            paddingValues = paddingValues,
+                            savedMovieId = savedMovieId,
+                            onMovieIdSaved = { savedMovieId = it }
+                        )
+                    }
+                    Screen.Settings -> key("settings") {
+                        SettingsScreen(
+                            movieViewModel = movieViewModel,
+                            authViewModel = authViewModel,
+                            paddingValues = paddingValues
+                        )
+                    }
+                }
+            }
         }
-
     }
 }
